ce6fcd3f5ae3e1556f596c123cbbbe487ecbb070
viniciusbe <vinicius.bernardesds@gmail.com>
2021-04-10 22:18:39 -0300

    refactor: remove chakra theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,10 @@ import { ChakraProvider } from '@chakra-ui/react';
 
 import { AppProps } from 'next/app';
 import { AuthProvider } from '../hooks/auth';
-import theme from '../theme';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <ChakraProvider resetCSS theme={theme}>
+    <ChakraProvider resetCSS>
       <AuthProvider>
         <Component {...pageProps} />
       </AuthProvider>
